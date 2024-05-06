5fd4453566b26d7ab047c1510b6ee4b4999d89c0
Adam Chomicki <admchm@outlook.com>
2024-05-06 17:10:10 +0200

    Fetch the main page of wikipedia

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-from bs4 import BeautifulSoup
-from urllib import request
-import os
+#import requests
+import requests
 
-ttb_url = "https://web.archive.org/web/20230906095156/https://www.ttb.gov/beer/statistics"
+page_url = 'https://en.wikipedia.org/wiki/Main_Page'
+page = requests.get(page_url)
 
-if not os.path.exists("ttb_stats.json"):
-    with open("ttb_page.html", "wb") as f:
-        resp = request.urlopen(ttb_url)
-        body = resp.read()
-        f.write(body)
-else:
-    with open("ttb_page.html") as f:
-        body = f.read()
+print(page.content)
 
-# change this to lxml intead of html5lib if you can't use it
-soup = BeautifulSoup(body, "html5lib")
-print(soup.title)
+# from bs4 import BeautifulSoup
+# from urllib import request
+# import os
+
+# ttb_url = "https://web.archive.org/web/20230906095156/https://www.ttb.gov/beer/statistics"
+
+# if not os.path.exists("ttb_stats.json"):
+#     with open("ttb_page.html", "wb") as f:
+#         resp = request.urlopen(ttb_url)
+#         body = resp.read()
+#         f.write(body)
+# else:
+#     with open("ttb_page.html") as f:
+#         body = f.read()
+
+# # change this to lxml intead of html5lib if you can't use it
+# soup = BeautifulSoup(body, "html5lib")
+# print(soup.title)
