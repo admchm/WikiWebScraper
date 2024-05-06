a2706b11d5f46fe55f3d3f5cd92c1183840470d7
Adam Chomicki <admchm@outlook.com>
2024-05-06 18:35:35 +0200

    Count links on a webpage. Clean up the file

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-#import requests
+from bs4 import BeautifulSoup
 import requests
 
 page_url = 'https://en.wikipedia.org/wiki/Main_Page'
 page = requests.get(page_url)
 
-print(page.content)
+soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.prettify())
 
-# from bs4 import BeautifulSoup
-# from urllib import request
-# import os
-
-# ttb_url = "https://web.archive.org/web/20230906095156/https://www.ttb.gov/beer/statistics"
-
-# if not os.path.exists("ttb_stats.json"):
-#     with open("ttb_page.html", "wb") as f:
-#         resp = request.urlopen(ttb_url)
-#         body = resp.read()
-#         f.write(body)
-# else:
-#     with open("ttb_page.html") as f:
-#         body = f.read()
-
-# # change this to lxml intead of html5lib if you can't use it
-# soup = BeautifulSoup(body, "html5lib")
-# print(soup.title)
+# 1. Counting all of the tags from the webpage
+links_count = soup.find_all('a')
+print(f'found {len(links_count)} links')
