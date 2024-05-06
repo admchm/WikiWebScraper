e07edc88850526e888be94ac7507c12be5ec8539
Adam Chomicki <admchm@outlook.com>
2024-05-06 22:12:07 +0200

    Resolve the issue with appending wrong items

--- list.py
+++ list.py
@@ -1,5 +1,3 @@
-#   https://en.wikipedia.org/wiki/Category:Super_Nintendo_Entertainment_System_games
-
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -11,7 +9,15 @@ urls = ["https://en.wikipedia.org/wiki/Category:Super_Nintendo_Entertainment_Sys
         "https://en.wikipedia.org/w/index.php?title=Category:Super_Nintendo_Entertainment_System_games&pagefrom=Super+Bonk#mw-pages",
         "https://en.wikipedia.org/w/index.php?title=Category:Super_Nintendo_Entertainment_System_games&pagefrom=Wwf+Wrestlemania%3A+The+Arcade+Game%0AWWF+WrestleMania%3A+The+Arcade+Game#mw-pages"]
 
-to_be_excluded = ["Wikipedia:", "Help:", "Category:", "List_of_Super_Nintendo_Entertainment_System_games:"]
+def keyword_not_forbidden(url):
+    forbidden_keyword = ["Wikipedia:", "Help:", "Category:", "Special:", "Portal:", "Main_Page", "List_of_Super_Nintendo_Entertainment_System_games:"]
+    
+    for keyword in forbidden_keyword:
+        if keyword in url:
+            return True
+            
+    return False
+
 
 SNES_games = []
 start = time.time()
@@ -21,14 +27,13 @@ for url in urls:
     soup = BeautifulSoup(page.content, 'html.parser')
 
     for element in soup.find_all('a', href=True):
-        if element['href'][0:6]=='/wiki/': 
-            for exception in to_be_excluded:
-                if exception not in element['href']:
-                    print(element['href'])
-                    #SNES_games.append(element['href'])
+        item = element['href']
+        if item[0:6]=='/wiki/': 
+            #TODO: bad naming convention, to be fixed
+            if not keyword_not_forbidden(item):
+                SNES_games.append(item)
 
-
-#for url in SNES_games:
-#    print(url)
+for url in SNES_games:
+    print(url)
 
 print(f'Edited {len(SNES_games)} games for {round(time.time()-start,1)} seconds')
