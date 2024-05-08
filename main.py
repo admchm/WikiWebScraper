3f80c867d6cd6746d5bf1e55a717b14c6be2b5ce
Adam Chomicki <admchm@outlook.com>
2024-05-08 15:21:29 +0200

    Implement solution for getting release dates data when wikipage contains info about multiplatforms

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
+import re
+
 from models.Item import Item
 from models.WikiHandler import WikiHandler
 
@@ -23,16 +25,56 @@ for url in urls:
     #getting correct and full name
     table_content = result[0].contents[0]    
     single_item = Item(title = table_content.contents[0].get_text(), platform = "SNES")
+    #print(table_content.get_text())
+    
+    #getting all release dates for all platforms
+    trimmed_text = re.sub(r'.*?(?=Super NES|SNES|Super Famicom)', '', table_content.get_text(), flags=re.S)
+    trimmed_text = re.sub(r'.*?(?=Release)', '', trimmed_text, flags=re.S)
+    trimmed_text = re.sub(r'Genre.*$', '', trimmed_text, flags=re.S)
+    
+    print(trimmed_text)
+    
+    pattern = r"(Super NES|SNES|Super Famicom)(?:((?:JP|NA|PAL): \w+ \d{1,2}, \d{4}))+"
+    matches = re.finditer(pattern, trimmed_text)
+
+    results = {}
+    for match in matches:
+        console_name = match.group(1)
+        data_matches = re.findall(r"(JP|NA|PAL): (\w+ \d{1,2}, \d{4})", match.group(0))
+        results[console_name] = data_matches
+
+    for console, dates in results.items():
+        print(f"{console}:")
+        for region, date in dates:
+            print(f"  {region}: {date}")
+    
+    # https://en.wikipedia.org/wiki/Frogger
+    # <a href="/wiki/North_America" title="North America">NA</a>:</span> September 1981
+    
+    # https://en.wikipedia.org/wiki/Chrono_Trigger
+    #<b>Super NES</b><link href="mw-data:TemplateStyles:r1126788409" rel="mw-deduplicated-inline-style"/><div class="plainlist"><ul><li><span style="font-size:97%;"><a href="/wiki/Japan" title="Japan">JP</a>:</span> March 11, 1995</li><li><span style="font-size:97%;"><a href="/wiki/North_America" title="North America">NA</a>:</span> August 11, 1995</li></ul></div>
+    
+     
+    # for child in table_content:
+    #    #getting release dates
+    #    text = child.get_text()
        
-    for child in table_content:
-       #getting release dates
-       text = child.get_text()
-       
-       if "Release" in text:
-               print(text)
-               if "Super NES" or "SNES" or "Super Famicom":
-                   single_item.get_dates(text)
-                   single_item.show_item_details()    
+    #    if "Release" in text:
+    #            #print(text)
+    #            print(child.get_text())
+    #            if "Super NES" or "SNES" or "Super Famicom" in text:
+    #                 print("Exists")
+    #            else:
+    #                print("Don't exist")
+               
+    #            #if "Super NES" or "SNES" or "Super Famicom" in text:
+    #                #print(text)
+    #                #single_item.get_dates(text)
+    #                #single_item.show_item_details()
+    #                #print("")
+        
+        
+        
         
         #print(child.get_text())
         
