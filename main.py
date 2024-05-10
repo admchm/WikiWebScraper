8fd94d92ec47e2cc58c455cd80b37c3de164f597
Adam Chomicki <admchm@outlook.com>
2024-05-10 12:16:06 +0200

    Refactor main file by splitting the functionality

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ from models.TextResources import TextRes
 shared = WikiHandler()
 start = time.time()
 
+def get_article_data_from_table(element):
+    url = TextRes.get_wiki_base_adress() + element[TextRes.get_href()]
+    page_details = requests.get(url)
+                
+    soup = BeautifulSoup(page_details.content, TextRes.get_html_attrib())
+    result = soup.find_all(class_ = TextRes.get_wiki_game_details_table_name())
+    return url,result
+
+def propagate_data(shared, table_content, single_item):
+    single_item.prepare_dates(table_content.get_text())
+    single_item.show_item_details()
+                    
+    shared.SNES_games_list.append(single_item)
+
+def increment_counter_if_dates_are_empty(shared, single_item):
+    if single_item.release_JP==TextRes.empty_string and single_item.release_NA==TextRes.empty_string and single_item.release_JP==TextRes.empty_string:
+        shared.counter += 1
+
 for url in shared.urls:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, TextRes.get_html_attrib())
@@ -19,22 +37,14 @@ for url in shared.urls:
         if url_item_name[0:6]==TextRes.get_wiki_endpoint():
              
             if not shared.forbidden(url_item_name):
-                url = TextRes.get_wiki_base_adress() + element[TextRes.get_href()]
-                page_details = requests.get(url)
-                
-                soup = BeautifulSoup(page_details.content, TextRes.get_html_attrib())
-                result = soup.find_all(class_ = TextRes.get_wiki_game_details_table_name())
+                url, result = get_article_data_from_table(element)
                 
                 if result:
                     table_content = result[0].contents[0]    
                     single_item = Item(title = table_content.contents[0].get_text(), game_url= url)
-                    single_item.prepare_dates(table_content.get_text())
-                    single_item.show_item_details()
+                    propagate_data(shared, table_content, single_item)
                     
-                    shared.SNES_games_list.append(single_item)
-                    
-                    if single_item.release_JP==TextRes.empty_string and single_item.release_NA==TextRes.empty_string and single_item.release_JP==TextRes.empty_string:
-                        shared.counter += 1
+                    increment_counter_if_dates_are_empty(shared, single_item)
                     
 print(f"Edited {len(shared.SNES_games_list)} games for {round(time.time()-start,1)} seconds")
 
