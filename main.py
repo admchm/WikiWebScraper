e9ebe8c3ae98af8929899767a8e96259915b207e
Adam Chomicki <admchm@outlook.com>
2024-05-14 13:46:51 +0200

    Include basic error handling into the project

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 import asyncio
 import aiohttp
+
+from aiohttp import ClientError
 from bs4 import BeautifulSoup
 
 from ItemDataProcessor import ItemDataProcessor
@@ -9,16 +11,27 @@ from WikiHandler import WikiHandler
 from models.TextResources import TextRes
 
 async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+    try:
+        async with session.get(url) as response:
+            response.raise_for_status() # Throw an exception if HTTP status will be 4xx/5xx
+            return await response.text()
+        
+    except (ClientError, aiohttp.TimeoutError) as e:
+        print(f"Error fetching URL {url}: {str(e)}")
+        return None
 
 async def get_article_data_from_table(session, element):
     url = TextRes.get_wiki_base_adress() + element[TextRes.get_href()]
-    page_details = await fetch(session, url)
-            
-    soup = BeautifulSoup(page_details, TextRes.get_html_attrib())
-    result = soup.find_all(class_ = TextRes.get_wiki_game_details_table_name())
-    return url, result
+    
+    try: 
+        page_details = await fetch(session, url)
+        soup = BeautifulSoup(page_details, TextRes.get_html_attrib())
+        result = soup.find_all(class_ = TextRes.get_wiki_game_details_table_name())
+        return url, result
+    
+    except Exception as e:
+        print(f"Error processing data from table for article: {str(e)}")
+        return None
 
 def propagate_data(wiki_handler, table_content, single_item, processor):
     data_from_wiki = table_content.get_text()
@@ -42,6 +55,11 @@ async def main():
         
         for url in wiki_handler.urls:
             page_content = await fetch(session, url)
+            
+            if page_content is None:
+                print("ERROR: page_content is empty!")
+                continue
+            
             soup = BeautifulSoup(page_content, TextRes.get_html_attrib())
 
             for element in soup.find_all('a', href=True):
@@ -49,18 +67,24 @@ async def main():
                 if url_item_name[0:6]==TextRes.get_wiki_endpoint() and not wiki_handler.forbidden(url_item_name):
                         tasks.append(get_article_data_from_table(session, element))
                         
-                        
-        results = await asyncio.gather(*tasks)
-        
-        for url, result in results:
-            if result:
-                table_content = result[0].contents[0]    
-                single_item = Item(title = table_content.contents[0].get_text(), game_url= url)
-                processor = ItemDataProcessor()
+        try:                        
+            results = await asyncio.gather(*tasks, return_exceptions=True)
+            for url, result in results:
+                #if result is None or isinstance(result, Exception):
                 
-                propagate_data(wiki_handler, table_content, single_item, processor)
-                            
-                increment_counter_if_dates_are_empty(wiki_handler, single_item)
+                if result and result[0]: #result might be empty at this point, because table in article might be collapsed
+                    if result is None or isinstance(result, Exception):
+                        continue
+                
+                    table_content = result[0].contents[0]    
+                    single_item = Item(title = table_content.contents[0].get_text(), game_url= url)
+                    processor = ItemDataProcessor()
+                
+                    propagate_data(wiki_handler, table_content, single_item, processor)
+                    increment_counter_if_dates_are_empty(wiki_handler, single_item)
+            
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
                             
         print(f"Edited {len(wiki_handler.SNES_games_list)} games for {round(time.time()-start,1)} seconds")
         wiki_handler.calculate_objects_count()
