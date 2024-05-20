803781c717d3f412ca1849fc81a811e261748461
Adam Chomicki <admchm@outlook.com>
2024-05-20 21:10:59 +0200

    Write unit tests for verifying file results for specific columns

--- tests/csv_creator_test.py
+++ tests/csv_creator_test.py
@@ -5,6 +5,9 @@ from CSVCreator import CSVCreator
 from models.Item import Item
 
 class TestCSVCreator:
+    
+    #file structure: Title - Platform - NA release - PAL release - JP release - Wiki URL
+    
     def test_combine_path_with_file_name(self):
         csv_creator = CSVCreator()
         csv_creator.set_path("~/")
@@ -25,7 +28,7 @@ class TestCSVCreator:
         list_of_items = []
         
         item_first = Item("The Addams Family", release_NA="December 01, 1992", release_PAL="———", release_JP="———", game_url="https://en.wikipedia.org/wiki/The_Addams_Family_(video_game)")
-        item_second = Item("The Addams FamilyAsterix & Obelix", release_NA="———", release_PAL="December 01, 1995", release_JP="———", game_url="https://en.wikipedia.org/wiki/Asterix_%26_Obelix_(video_game)")
+        item_second = Item("Asterix & Obelix", release_NA="———", release_PAL="December 01, 1995", release_JP="———", game_url="https://en.wikipedia.org/wiki/Asterix_%26_Obelix_(video_game)")
         item_third = Item("Bill Walsh College Football", release_NA="February 01, 1994", release_PAL="———", release_JP="———", game_url="https://en.wikipedia.org/wiki/Bill_Walsh_College_Football")
         
         list_of_items.append(item_first)
@@ -60,6 +63,84 @@ class TestCSVCreator:
             expected_headers = ["Title", "Platform", "NA release", "PAL release", "JP release", "Wiki URL"]
             assert headers == expected_headers
 
-#check if file contains specific game titles
-#check if file contains dates for specific titles
-#check if file contains links
+    def test_presence_of_specific_title_in_first_column(self):
+        csv_creator = CSVCreator()
+        
+        list_of_items = []
+        
+        item_first = Item("The Addams Family", release_NA="December 01, 1992", release_PAL="———", release_JP="———", game_url="https://en.wikipedia.org/wiki/The_Addams_Family_(video_game)")
+        item_second = Item("Asterix & Obelix", release_NA="———", release_PAL="December 01, 1995", release_JP="———", game_url="https://en.wikipedia.org/wiki/Asterix_%26_Obelix_(video_game)")
+        item_third = Item("Bill Walsh College Football", release_NA="February 01, 1994", release_PAL="———", release_JP="———", game_url="https://en.wikipedia.org/wiki/Bill_Walsh_College_Football")
+        
+        list_of_items.append(item_first)
+        list_of_items.append(item_second)
+        list_of_items.append(item_third)
+        
+        csv_creator = CSVCreator()
+        csv_creator.prepare_file(list_of_items)
+        
+        with open(csv_creator.path_combined, mode='r') as file:
+            reader = csv.reader(file)
+            headers = next(reader) #skipping headers
+            found = False
+            
+            for row in reader:
+                if row[0] == "Bill Walsh College Football":
+                    found = True
+                    break
+            assert found, "Bill Walsh College Football not found in the first column"
+
+    def test_presence_of_specific_date_in_column(self):
+        csv_creator = CSVCreator()
+        
+        list_of_items = []
+        
+        item_first = Item("The Addams Family", release_NA="December 01, 1992", release_PAL="———", release_JP="———", game_url="https://en.wikipedia.org/wiki/The_Addams_Family_(video_game)")
+        item_second = Item("Asterix & Obelix", release_NA="———", release_PAL="December 01, 1995", release_JP="———", game_url="https://en.wikipedia.org/wiki/Asterix_%26_Obelix_(video_game)")
+        item_third = Item("Bill Walsh College Football", release_NA="February 01, 1994", release_PAL="———", release_JP="———", game_url="https://en.wikipedia.org/wiki/Bill_Walsh_College_Football")
+        
+        list_of_items.append(item_first)
+        list_of_items.append(item_second)
+        list_of_items.append(item_third)
+        
+        csv_creator = CSVCreator()
+        csv_creator.prepare_file(list_of_items)
+        
+        with open(csv_creator.path_combined, mode='r') as file:
+            reader = csv.reader(file)
+            headers = next(reader)
+            found = False
+            
+            for row in reader:
+                if row[2] == "February 01, 1994":
+                    found = True
+                    break
+            assert found, "February 01, 1994 not found in the third column"
+            
+    def test_presence_of_specific_link_in_last_column(self):
+        csv_creator = CSVCreator()
+        
+        list_of_items = []
+        
+        item_first = Item("The Addams Family", release_NA="December 01, 1992", release_PAL="———", release_JP="———", game_url="https://en.wikipedia.org/wiki/The_Addams_Family_(video_game)")
+        item_second = Item("Asterix & Obelix", release_NA="———", release_PAL="December 01, 1995", release_JP="———", game_url="https://en.wikipedia.org/wiki/Asterix_%26_Obelix_(video_game)")
+        item_third = Item("Bill Walsh College Football", release_NA="February 01, 1994", release_PAL="———", release_JP="———", game_url="https://en.wikipedia.org/wiki/Bill_Walsh_College_Football")
+        
+        list_of_items.append(item_first)
+        list_of_items.append(item_second)
+        list_of_items.append(item_third)
+        
+        csv_creator = CSVCreator()
+        csv_creator.prepare_file(list_of_items)
+        
+        with open(csv_creator.path_combined, mode='r') as file:
+            reader = csv.reader(file)
+            headers = next(reader)
+            found = False
+            
+            for row in reader:
+                if row[5] == "https://en.wikipedia.org/wiki/Asterix_%26_Obelix_(video_game)":
+                    found = True
+                    break
+            assert found, "https://en.wikipedia.org/wiki/Asterix_%26_Obelix_(video_game) not found in the sixth column"
+            
