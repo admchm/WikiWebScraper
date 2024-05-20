89f86f221a498cbb91a3029e45c0151a3fb27bdd
Adam Chomicki <admchm@outlook.com>
2024-05-20 12:48:27 +0200

    Add unit tests to verify file creation and deletion for CSVCreator

--- tests/csv_creator_test.py
+++ tests/csv_creator_test.py
@@ -17,11 +17,21 @@ class TestCSVCreator:
         csv_creator.combine_path_with_file_name()
         
         assert csv_creator.path_combined == os.path.expanduser("~/SNES_games_list.csv")
-               
-        
     
+    def test_prepare_file(self):
+        csv_creator = CSVCreator()
+        csv_creator.prepare_file()
+        
+        assert os.path.exists(csv_creator.path_combined)
+        
+    def test_access_removed_file(self):
+        csv_creator = CSVCreator()
+        
+        if os.path.exists(csv_creator.path_combined):
+            os.remove(csv_creator.path_combined)
+        
+        assert not os.path.exists(csv_creator.path_combined)
 
-#check if file is created
 #check if file contains columns
 #check if file contains specific game titles
 #check if file contains dates for specific titles
